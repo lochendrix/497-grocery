cecd106e6b90b6b63b9c688167cc77e938823094
Lochlan Hendrix <lhendrix@umich.edu>
2025-03-21 21:01:00 -0400

    added Jonathan's mock code to front page, updated structure

--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -8,12 +8,16 @@ db = SQLAlchemy(app)
 # Import the Kroger service function
 from services.kroger import get_store_info
 
-@app.route('/')
-def index():
+@app.route('/kroger')
+def kroger():
     # Call the Kroger API code and get the result string
     store_info = get_store_info()
     # For mockup purposes, just return the string
     return f"<pre>{store_info}</pre>"
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
